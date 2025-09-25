b3d79d45f6b38711dc829ed6fc52894320cbd429
Gerard Balaoro <gerardbalaoro@gmail.com>
2025-09-25 12:51:10 +0800

    feat(scripts): add installation script for devtools

---
+++ scripts/windows/install-helpers.ps1
@@ -0,0 +1,34 @@
+function Get-Package {
+    param (
+        [string]$PackageId
+    )
+
+    $result = winget list --id $PackageId -e 2>$null
+    return $result -match $PackageId
+}
+
+function Install-Package {
+    param (
+        [string]$id
+    )
+
+    if (Get-Package $id) {
+        Write-Output "$id is already installed."
+    } else {
+        Write-Output "Installing $id..."
+        winget install --id=$id -e --silent --accept-package-agreements --accept-source-agreements
+    }
+}
+
+function Install-Script {
+    param (
+        [string]$exe,
+        [string]$uri
+    )
+
+    if (Get-Command $exe -ErrorAction SilentlyContinue) {
+        Write-Output "$exe is already installed."
+    } else {
+        powershell -ExecutionPolicy ByPass -c "irm $uri | iex"
+    }
+}
