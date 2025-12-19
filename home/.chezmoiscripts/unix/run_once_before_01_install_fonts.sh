ee0efa0b57e0179526d81b391fff7693786c9140
Gerard Balaoro <gerardbalaoro@gmail.com>
2025-12-19 15:16:30 +0800

    feat: update fonts

--- home/.chezmoiscripts/unix/run_once_before_01_install_fonts.sh
+++ home/.chezmoiscripts/unix/run_once_before_01_install_fonts.sh
@@ -3,7 +3,8 @@
 if command -v oh-my-posh >/dev/null; then
   chezmoi git lfs install
   chezmoi git lfs pull
-  oh-my-posh font install "$HOME/.local/share/chezmoi/data/fonts/Monaspace Neon.zip"
+  oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
+  oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
 else
   echo "oh-my-posh not found, skipping font installation"
 fi
--- home/.chezmoiscripts/windows/run_once_before_03_install_fonts.ps1
+++ home/.chezmoiscripts/windows/run_once_before_03_install_fonts.ps1
@@ -1,7 +1,8 @@
 if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) {
     chezmoi git lfs install
     chezmoi git lfs pull
-    oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/Monaspace Neon.zip"
+    oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
+    oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
 } else {
     Write-Host "oh-my-posh not found, skipping font installation"
 }
