380b2395b6618370499a37a9e145bc5277d4366f
Gerard Balaoro <gerardbalaoro@gmail.com>
2026-04-04 01:28:05 +0800

    refactor: clean up legacy scripts and fix script issues

--- home/.chezmoiscripts/unix/run_once_before_03_install_fonts.sh
+++ home/.chezmoiscripts/unix/run_once_before_03_install_fonts.sh
@@ -3,8 +3,8 @@
 if command -v oh-my-posh >/dev/null; then
   chezmoi git lfs install
   chezmoi git lfs pull
-  oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
-  oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
+  oh-my-posh font install "$CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
+  oh-my-posh font install "$CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
 else
   echo "oh-my-posh not found, skipping font installation"
 fi
