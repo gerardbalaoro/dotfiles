e75a4ec321e51c8e099a7f40a934a5b9b5b28346
Gerard Balaoro <gerardbalaoro@gmail.com>
2026-05-21 23:01:52 +0800

    refactor(fonts): generalize font installation to install all zip files

--- home/.chezmoiscripts/unix/run_once_before_03_install_fonts.sh
+++ home/.chezmoiscripts/unix/run_once_before_03_install_fonts.sh
@@ -3,8 +3,9 @@
 if command -v oh-my-posh >/dev/null; then
   chezmoi git lfs install
   chezmoi git lfs pull
-  oh-my-posh font install "$CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
-  oh-my-posh font install "$CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
+  for font in "$CHEZMOI_WORKING_TREE/data/fonts"/*.zip; do
+    oh-my-posh font install "$font"
+  done
 else
   echo "oh-my-posh not found, skipping font installation"
 fi
--- home/.chezmoiscripts/windows/run_once_before_03_install_fonts.ps1
+++ home/.chezmoiscripts/windows/run_once_before_03_install_fonts.ps1
@@ -1,8 +1,9 @@
 if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) {
     chezmoi git lfs install
     chezmoi git lfs pull
-    oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/GeistMonoNF.zip"
-    oh-my-posh font install "$env:CHEZMOI_WORKING_TREE/data/fonts/MonaspaceNeonNF.zip"
+    Get-ChildItem "$env:CHEZMOI_WORKING_TREE/data/fonts/*.zip" | ForEach-Object {
+        oh-my-posh font install $_.FullName
+    }
 } else {
     Write-Host "oh-my-posh not found, skipping font installation"
 }
