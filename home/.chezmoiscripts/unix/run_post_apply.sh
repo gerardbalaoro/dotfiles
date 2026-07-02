990e06cfd4a2a6966bfe543bdfb338fde63a2e12
Gerard Balaoro <gerardbalaoro@gmail.com>
2026-07-02 23:47:52 +0800

    feat(shell): add shell profiles and prompt configuration

--- home/.chezmoiscripts/unix/run_post_apply.sh
+++ home/.chezmoiscripts/unix/run_post_apply.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+set -eu
+
+# Remove competing shell profiles
+rm -f "$HOME/.bash_profile" "$HOME/.bash_login" "$HOME/.zprofile"
+
 # Install mise packages
 if command -v mise >/dev/null 2>&1; then
-    mise install
+  mise install
 fi
+
+# Generate shell integrations
+. "$HOME/.profile"
+install_shell_integrations
