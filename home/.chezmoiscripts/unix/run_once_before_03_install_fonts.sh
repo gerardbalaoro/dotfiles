2c8e3c8f179c8a1bdd6ef57fbef581561f419d8d
Gerard Balaoro <gerardbalaoro@gmail.com>
2026-07-02 23:48:39 +0800

    feat(fonts): add Commit Mono fonts and install scripts

---
+++ home/.chezmoiscripts/unix/run_once_before_03_install_fonts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if command -v oh-my-posh >/dev/null; then
+  chezmoi git lfs install
+  chezmoi git lfs pull
+
+  fonts=("CommitMono" "CommitMono_NerdFont")
+  for font in "${fonts[@]}"; do
+    oh-my-posh font install "$CHEZMOI_WORKING_TREE/data/fonts/$font.zip"
+  done
+else
+  echo "oh-my-posh not found, skipping font installation"
+fi
---
+++ home/.chezmoiscripts/windows/run_once_before_03_install_fonts.ps1
@@ -0,0 +1,14 @@
+if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) {
+    chezmoi git lfs install
+    chezmoi git lfs pull
+
+    $fonts = @("CommitMono", "CommitMono_NerdFont")
+    foreach ($font in $fonts) {
+        $fontPath = Join-Path $env:CHEZMOI_WORKING_TREE "data/fonts/$font.zip"
+        if (Test-Path $fontPath) {
+            oh-my-posh font install $fontPath
+        }
+    }
+} else {
+    Write-Host "oh-my-posh not found, skipping font installation"
+}
